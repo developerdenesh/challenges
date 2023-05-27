ab46424ab5b6d037750ec656ac2ac5aaae7b061f
Den <denesh@u.nus.edu>
2023-05-27 19:19:33 +0800

    Adding greedy search based on occupancy grid maps

---
+++ Grind/greedy_search.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+
+#define max_size 5
+
+std::pair<int, int> one = std::make_pair(1, 0);
+std::pair<int, int> two = std::make_pair(-1, 0);
+std::pair<int, int> three = std::make_pair(0, 1);
+std::pair<int, int> four = std::make_pair(0, -1);
+std::vector<std::pair<int, int>> search = {one, two, three, four};
+std::vector<std::pair<int, int>> memory;
+
+struct Colors
+{
+    enum
+    {
+        black = 0,
+        white = 1
+    };
+};
+
+bool compute(std::vector<std::vector<int>> &map, std::pair<int, int> start, std::pair<int, int> &end, std::vector<std::pair<int, int>> &path, int &distance)
+{
+    // Can't go beyond the edge of the map
+    if (start.first < 0 || start.first + 1 > max_size || start.second < 0 || start.second + 1 > max_size)
+        return false;
+
+    if (std::find(memory.begin(), memory.end(), start) != memory.end())
+        return false;
+
+    // Can't go through obstacles
+    if (map[start.first][start.second] == Colors::black)
+        return false;
+
+    // Robot reached the end
+    if (start.first == end.first && start.second == end.second)
+    {
+        // TODO: make use of this distance
+        distance = 0;
+        return true;
+    }
+
+    memory.push_back(start);
+
+    // Perform depth first search on the map until reaching the location
+    // This is greedy (for a comprehensive search compare the lengths of the paths)
+    std::pair<int, int> new_start;
+    for (int i = 0; i < search.size(); i++)
+    {
+        new_start.first = start.first + search[i].first;
+        new_start.second = start.second + search[i].second;
+
+        if (compute(map, new_start, end, path, distance))
+        {
+            path.push_back(new_start);
+            distance++;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+std::vector<std::vector<int>> createMap()
+{
+    std::vector<std::vector<int>> map;
+    for (unsigned int i = 0; i < max_size; i++)
+    {
+        std::vector<int> temp_row;
+        for (unsigned int j = 0; j < max_size; j++)
+        {
+            temp_row.push_back(Colors::white);
+        }
+
+        map.push_back((temp_row));
+    }
+
+    return map;
+}
+
+void populateObstacles(std::vector<std::vector<int>> &map)
+{
+    for (int i = 0; i < map.size(); i++)
+    {
+        for (int j = 0; j < map[i].size(); j++)
+        {
+            if (i == 2 && j == 3)
+            {
+                map[i][j] = Colors::black;
+                continue;
+            }
+
+            if (i == 3 && j == 3)
+            {
+                map[i][j] = Colors::black;
+                continue;
+            }
+
+            if (i == 4 && j == 3)
+            {
+                map[i][j] = Colors::black;
+                continue;
+            }
+
+            if (i == 2 && j == 4)
+            {
+                map[i][j] = Colors::black;
+                continue;
+            }
+
+            if (i == 3 && j == 4)
+            {
+                map[i][j] = Colors::black;
+                continue;
+            }
+
+            if (i == 4 && j == 4)
+            {
+                map[i][j] = Colors::black;
+                continue;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    // input
+    // 1. Map
+    // 2. start,
+    // 3. end
+
+    // input 1.
+    std::vector<std::vector<int>> map = createMap();
+    // populateObstacles(map);
+
+    std::pair<int, int> start = std::make_pair(2, 1);
+    std::pair<int, int> end = std::make_pair(4, 4);
+
+    // output
+    // 1. path
+
+    std::vector<std::pair<int, int>> path;
+    int distance = 0;
+    compute(map, start, end, path, distance);
+
+    std::reverse(path.begin(), path.end());
+    for (int i = 0; i < path.size(); i++)
+    {
+        std::cout << "the path is x:" << path[i].first << ", y:" << path[i].second << std::endl;
+    }
+
+    return 0;
+}
